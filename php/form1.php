0e55cc9b8503c1e23192953513e0cdba01629d2e
Sergio Sepulveda <so.sepulveda.m@gmail.com>
2019-03-12 17:29:49 -0300

    commit final del viernes, problemas con validacion (pattern) en los input texto, revisar

--- php/form1.php
+++ php/form1.php
@@ -36,35 +36,83 @@
                             <br>
 
                             <div class="form-group">
-                                <p class="label">RUT</p>
-                                <input type="text" name="rut"  id="inputRut" required="" onblur="javascript:Rut(document.form1.rut.value)" class=" input-oscuro" placeholder="11111111-1"  autofocus="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+
+                                <div class="row">    
+                                        <div class="input-field col s12">
+                                                <input type="text" name="rut"  id="inputRut" required="" onblur="javascript:Rut(document.form1.rut.value)" class=" input-oscuro"  autofocus="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+                                                <label for="rut">RUT</label>
+                                                <span class="abajito" data-error="wrong" data-success="right">Sin puntos ni guión</span>
+                                        </div>        
+                                </div>
+
+                                <!-- Separador de campos -->
+
+                                <div class="row">    
+                                        <div class="input-field col s12">
+                                                <input type="text" name="ingresonombre" class="input-oscuro" pattern="" required="">
+                                                <label for="ingresonombre">Nombre</label>
+                                                <span class="abajito" data-error="wrong" data-success="right">Un nombre es suficiente</span>
+                                        </div>        
+                                </div>
+                                <!-- Separador de campos -->
+                                <div class="row">    
+                                        <div class="input-field col s12">
+                                                <input type="text" name="ingresapellidop" class="input-oscuro"  required="">
+                                                <label for="ingresapellidop">Apellido Paterno</label>
+                                                <span class="abajito" data-error="wrong" data-success="right">Primer apellido</span>
+                                        </div>        
+                                </div>
+
+                                <!-- Separador de campos -->
+
+                                <div class="row">    
+                                        <div class="input-field col s12">
+                                                <input type="text" name="ingresapellidom" class="input-oscuro" required="">
+                                                <label for="ingresapellidom">Apellido Materno</label>
+                                                <span class="abajito" data-error="wrong" data-success="right">Segundo apellido</span>
+                                        </div>        
+                                </div>       
+                                <!-- Separador de campos -->
+                                <div class="row">    
+                                        <div class="input-field col s12">
+                                                        <input type="date" class="input-oscuro" name="fechanacimiento" placeholder="" min="1919-01-01" required>
+                                                <label for="fechanacimiento">Fecha de Nacimiento</label>
+                                                <span class="abajito" data-error="wrong" data-success="right">Se desplegará un menú cuando haga click</span>
+                                        </div>        
+                                </div> 
+
+                                <!-- Separador de campos -->
+                                <br>
+                                <h1 class="h2 mb-4 mt-1 font-weight-normal">Contacto</h1>
+                                <small>Esta información es muy importante, si necesitamos contactarte, lo haremos usando estos datos.</small>
+                                <br>
+                                <!-- Separador de campos -->
+
+                                 <div class="row">    
+                                         <div class="input-field col s12">
+                                                              
+
+                                                <input id="email" type="email" class="input-oscuro "  required>
+                                                <label for="email">Email</label>
+                                                <span class="abajito" data-error="wrong" data-success="right">Su correo actualizado</span>
+                                         </div>        
+                                </div> 
                                
-                                <p class="label">Nombre</p>
-                                <input type="text" class="input-oscuro" placeholder="Su nombre" required="" x-moz-errormessage="Por favor, especifique una dirección de correo válida.">
+                               <!-- Separador de campos -->
+
+                               <div class="row">    
+                                        <div class="input-field col s12 ">
+                                                        
+
+                                                <input id="telefono" type="tel" class="input-oscuro" pattern="[0-9]{9}" required>
+                                                <label for="telefono">Teléfono</label>
+                                                <span class="abajito" data-error="wrong" data-success="right">Desde el 9 en adelante  </span>
+                                        </div>        
+                               </div> 
+
                                 
-                                <p class="label">Apellido Paterno</p>
-                                <input type="text" class="input-oscuro" placeholder="Su Apellido Paterno">
-
-                                <p class="label">Apellido Materno</p>
-                                <input type="text" class="input-oscuro" placeholder="Su Apellido Materno">
-
-                                <p class="label">Fecha de Nacimiento</p>
-                                <input type="date" class="input-oscuro" placeholder="" min="1919-01-01">
-
-                                <p class="label">Sexo</p>
-                                <div class="input-field col-6">
-                                        <select required>
-                                          <option value="" disabled selected>Elije una Opcion</option>
-                                          <option value="No Especifica">Prefiero no decir</option>
-                                          <option value="Femenino">Femenino</option>
-                                          <option value="Masculino">Masculino</option>
-                                        </select>
-                                      </div>
-                                <select required >
-                                        
-                                        
-                                        
-                                </select> 
+                                                    
+                               
                               
                             </div>
 
@@ -80,7 +128,7 @@
                                         <div class="button__vertical"></div>
                             </button>
                             <br>
-                            <small class="">Tranquilo, son solo unos pocos datos, y solo los pediremos <b>una vez</b>.</small>
+                            <small class="">Tranquilo, son solo unos pocos datos, y solo los pediremos <b>una vez</b>. Tu proxima visita cargará estos datos automaticamente.</small>
 
                         
                     </form>
@@ -92,5 +140,12 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.js"></script>
+   
+    <script>
+             $(document).ready(function(){
+                $('select').formSelect();
+        });
+    </script>
+</body>
 </html>
